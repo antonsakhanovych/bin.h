b7b8e5ee671e03f7a31704967546a1a920514c00
Anton Sakhanovych <antonsakhanovych@gmail.com>
2023-12-02 13:33:21 +0100

    modify bin.h

--- bin.h
+++ bin.h
@@ -1,21 +1,24 @@
 #ifndef BINARY_HEAP
 #define BINARY_HEAP
+#define INITIAL_CAPACITY 3
 
 #include <stdlib.h>
 
 typedef struct {
-  void *_internal;
+  int *nodes;
+  size_t size;
+  size_t cap;
 } Binary_Heap;
 
 Binary_Heap *binheap_init();
 
 Binary_Heap *binheap_construct(int *arr);
 
-void binheap_insert(Binary_Heap *self);
+void binheap_insert(Binary_Heap *self, int val);
 
 int binheap_find_min(Binary_Heap *self);
 
-int binheap_del_mind(Binary_Heap *self);
+int binheap_del_min(Binary_Heap *self);
 
 Binary_Heap *binheap_merge(Binary_Heap *first, Binary_Heap *second);
 
